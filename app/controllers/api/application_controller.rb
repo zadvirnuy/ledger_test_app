09fda54cedc4d3bc3ff268735fe798405e048eaf
yuriy <yurij.zadvirnuy@gmail.com>
2020-04-25 21:30:35 +0300

    Add basic routes; ledger CRUD API operations

---
+++ app/controllers/api/application_controller.rb
@@ -0,0 +1,20 @@
+class Api::ApplicationController < ActionController::Base
+	skip_before_action :verify_authenticity_token, if: :json_request?
+	rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  def json_request?
+    request.format.json?
+  end
+
+	def response_success(success_message = nil, attributes = {})
+    { success: true, alert: success_message }.merge(attributes)
+  end
+
+  def response_fail(error_message = nil, attributes = {})
+    { success: false, alert: error_message }.merge(attributes)
+  end
+
+  def render_not_found
+    render json: response_fail('Not found.'), status: 404
+  end
+end
--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,3 +1,7 @@
 class Ledger < ActiveRecord::Base
   has_many :transactions
+
+  validates_presence_of :name, :starting_balance
+  validates_uniqueness_of :name
+  validates :starting_balance, numericality: { only_float: true }
 end
