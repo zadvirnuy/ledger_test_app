2a5ce8d2d50ce596a0ee3bd1ff4c3854760e9fde
yuriy <yurij.zadvirnuy@gmail.com>
2020-04-26 19:51:09 +0300

    add specs for models

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -13,21 +13,21 @@ class Ledger < ActiveRecord::Base
   	self.starting_balance + total_revenues - total_expenses
   end
 
-  def total_expenses(filter_params = nil)
-  	expenses_tr = self.transactions.where(tr_type: 'expenses')
-  	expenses_tr = filter_transactions_by(expenses_tr, filter_params) if filter_params
+  private
 
-  	expenses_tr.map(&:amount).reduce(0, :+)
+  def total_expenses(filter_params = nil)
+    expenses_tr = self.transactions.where(tr_type: 'expenses')
+    expenses_tr = filter_transactions_by(expenses_tr, filter_params) if filter_params
+
+    expenses_tr.map(&:amount).reduce(0, :+)
   end
 
   def total_revenues(filter_params = nil)
-  	revenues_tr = self.transactions.where(tr_type: 'revenues')
-  	revenues_tr = filter_transactions_by(revenues_tr, filter_params) if filter_params
+    revenues_tr = self.transactions.where(tr_type: 'revenues')
+    revenues_tr = filter_transactions_by(revenues_tr, filter_params) if filter_params
 
-  	revenues_tr.map(&:amount).reduce(0, :+)
+    revenues_tr.map(&:amount).reduce(0, :+)
   end
-
-  private
 
   def filter_transactions_by(tr, filter_params)
   	filtered = tr.where("CAST(strftime('%Y', date) as INT) = ?", filter_params[:year]) if filter_params[:year]
--- spec/models/ledger_spec.rb
+++ spec/models/ledger_spec.rb
@@ -1,4 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Ledger, type: :model do
+	context 'associations' do
+		it 'should have associations' do
+			is_expected.to have_many :transactions
+		end
+	end
+
+	context 'validations' do
+		it 'validates' do
+			is_expected.to validate_presence_of :name
+			is_expected.to validate_presence_of :starting_balance
+			is_expected.to validate_numericality_of(:starting_balance)
+		end
+	end
+
+	context '#methods' do
+		let!(:ledger) { create(:ledger) }
+		let!(:expenses_tr1) { create(:expenses_transaction, ledger: ledger) }
+		let!(:revenues_tr1) { create(:revenues_transaction, ledger: ledger) }
+		let!(:expenses_tr2) { create(:expenses_transaction, ledger: ledger) }
+		let!(:revenues_tr2) { create(:revenues_transaction, ledger: ledger) }
+
+		context 'totals' do
+			it 'returns ledger total expenses and revenues' do
+				total_expenses = expenses_tr1.amount + expenses_tr2.amount
+				total_revenues = revenues_tr1.amount + revenues_tr2.amount
+
+				expect(ledger.totals).to eq [total_expenses, total_revenues]
+			end
+		end
+
+		context 'balance' do
+			it 'returns ledger balance' do
+				total_expenses = expenses_tr1.amount + expenses_tr2.amount
+				total_revenues = revenues_tr1.amount + revenues_tr2.amount
+				calculated_balance = ledger.starting_balance + total_revenues - total_expenses
+
+				expect(ledger.balance).to eq calculated_balance
+			end
+		end
+	end
 end
